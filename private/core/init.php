8f6d6f7c21e2c9784dbf3375627ab10291fad5e9
NTaki5 <124534000+NTaki5@users.noreply.github.com>
2024-06-11 14:26:53 +0300

    Update init.php

--- private/core/init.php
+++ private/core/init.php
@@ -6,7 +6,7 @@ require "database.php";
 require "controller.php";
 require "app.php";
 require "model.php";
-require "sendEmail.php";
+// require "sendEmail.php";
 
 // inbuilt function, what is called, when a register, in our case a class is not found
 // for example in controllers/Home.php => $user = new User(); this function is called
